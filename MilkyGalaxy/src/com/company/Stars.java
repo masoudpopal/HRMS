6cf3c9d52726ef8ce059ec61dfbdadee00082c61
masoudpopal <masoudpopal123456@gmail.com>
2021-11-15 21:17:06 -0800

    11-15-21

---
+++ MilkyGalaxy/src/com/company/Stars.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Stars {
+    String earth;
+    String venus;
+    String jupiter;
+    String saturn;
+    int numbOfStars;
+    String mercury;
+    String size;
+
+}
---
+++ gitStashExamples/src/com/company/car.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class car {
+    public String color;
+    public String model;
+    public String doors;
+    public String name;
+    public String engineCapacity;
+    public String year;
+
+}
